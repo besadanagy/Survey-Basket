322f5669de19bc94cf8d6b9685dfbfc41bb2b6f7
besadanagy <besadanagy@email.com>
2025-06-20 18:54:24 +0300

    Add CRUD operations to Polls Controller

--- Survey Basket/GlobalUsings.cs
+++ Survey Basket/GlobalUsings.cs
@@ -3,3 +3,5 @@ global using Microsoft.AspNetCore.Mvc;
 
 global using Survey_Basket.Model;
 global using Survey_Basket.Repository;
+global using Microsoft.EntityFrameworkCore;
+global using Survey_Basket.Services;
--- Survey Basket/Program.cs
+++ Survey Basket/Program.cs
@@ -1,5 +1,3 @@
-using Microsoft.EntityFrameworkCore;
-using Survey_Basket.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
